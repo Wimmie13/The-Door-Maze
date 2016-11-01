9619db41db8699672c368bc78455fe8667ce0319
GIGCEEZ\Cees <gigceez@gmail.com>
2016-11-01 23:41:26 +0100

    fucking deur

--- The Door Maze/src/main/java/nl/han/ica/TheDoorMaze/Door.java
+++ The Door Maze/src/main/java/nl/han/ica/TheDoorMaze/Door.java
@@ -1,16 +1,19 @@
 package nl.han.ica.TheDoorMaze;
 
+import nl.han.ica.OOPDProcessingEngineHAN.Collision.ICollidableWithGameObjects;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
 
 import java.util.List;
 
-public class Door extends ActionObject{
+public class Door extends ActionObject implements ICollidableWithGameObjects {
 	private String naar;
+	private TheDoorMaze world;
 	private boolean keyNeeded;
 
 	public Door(TheDoorMaze world, int x, int y, String image, String naar, String itemName, boolean keyNeeded) {
 		super(world, image, itemName, x, y);
 		this.naar = naar;
+		this.world = world;
 		this.keyNeeded = keyNeeded;
 	}
 
@@ -18,23 +21,23 @@ public class Door extends ActionObject{
 		return this.naar;
 	}
 
-	public void keyPressed(int keyCode, char key) {
-	}
-
 	@Override
-	public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
-		for (GameObject g : collidedGameObjects) {
-			if (g instanceof Player && world.keyPressed) {
-				if(TheDoorMaze.inventory != null){
-					if (world.keyCode == TheDoorMaze.UP && TheDoorMaze.inventory.itemInInventory(this.naar) == true && keyNeeded == true) {
-						world.setNextMap(Integer.parseInt(this.naar));
-					} else if (world.keyCode == TheDoorMaze.UP && keyNeeded == false){
-						world.setNextMap(Integer.parseInt(this.naar));
-					} else if (world.keyCode == TheDoorMaze.UP){
-						Map.notify = new Notification(this.world, "Je hebt niet de juiste sleutel voor deze deur.");
-					}
-				}
-			}
-		}
+	 public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+	  for (GameObject g : collidedGameObjects) {
+	   if (g instanceof Player && world.keyPressed) {
+	    if(TheDoorMaze.inventory != null){
+	     if (world.keyCode == TheDoorMaze.UP && TheDoorMaze.inventory.itemInInventory(this.naar) == true && keyNeeded == true) {
+	      world.setNextMap(Integer.parseInt(this.naar));
+	     } else if (world.keyCode == TheDoorMaze.UP && keyNeeded == false){
+	      world.setNextMap(Integer.parseInt(this.naar));
+	     } else if (world.keyCode == TheDoorMaze.UP){
+	      Map.notify = new Notification(this.world, "Je hebt niet de juiste sleutel voor deze deur.");
+	     }
+	    }
+	   }
+	  }
 	}
 }
+	   
+	  
+
