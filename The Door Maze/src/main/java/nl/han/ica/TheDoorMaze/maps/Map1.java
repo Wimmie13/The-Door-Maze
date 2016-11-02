365f36a2f7f77276aa7e093d424dbae61b7eb635
GIGCEES\Cees <gigceez@gmail.com>
2016-11-02 09:14:03 +0100

    Kleine aanpassing aan map en op een of andere manier heel veel classes ofzo

--- The Door Maze/src/main/java/nl/han/ica/TheDoorMaze/maps/Map1.java
+++ The Door Maze/src/main/java/nl/han/ica/TheDoorMaze/maps/Map1.java
@@ -27,12 +27,13 @@ public class Map1 extends Map implements IMap{
 		this.objects.add(new Flower(this.world, 900, 200, "Bloem"));
 		this.objects.add(new Key(this.world, 1300, 250, "Key #2"));
 		this.objects.add(new Bookshelf(this.world, 1600, 170, "playboy"));
-		this.objects.add(new Person(world, 400, 200, "src/main/java/nl/han/ica/TheDoorMaze/media/objects/G001.png", "Henk",
+		this.objects.add(new Person(world, 400, 200, "src/main/java/nl/han/ica/TheDoorMaze/media/objects/G001.png", "Chris",
 				new String[]{
 						"Veel succes op je date broer, d'r insta looks boem spang,",
-						"net zoals team boem spang, maar dan minder spang."}));
+						"net zoals team boem spang, maar dan minder spang.",
+						"De code van de kluis van Ricky is trouwens 1234."}));
 		
-		((Person)this.objects.get(this.objects.size() - 1)).addMission("Fix een bloem voor Henk", "Bloem", 
+		((Person)this.objects.get(this.objects.size() - 1)).addMission("Fix een bloem voor Chris", "Bloem", 
 				new String[][] { 
 					{"Yo man, ik heb binnenkort een date zou je voor mij een bloem willen fixen?"},
 					{"Thx man! neem deze condoom, je zult hem nodig hebben ;-)"},
